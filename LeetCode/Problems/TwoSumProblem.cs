3435af911aa380688e63a2b087769dc050fde177
vul3yo6 <vul3yo6@hotmail.com>
2020-06-06 14:48:51 +0800

    create twoSum

---
+++ LeetCode/Problems/TwoSumProblem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    public class TwoSumProblem
+    {
+        /*
+         * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+         * 
+         * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+         * Example:
+         * 
+         * Given nums = [2, 7, 11, 15], target = 9,
+         * Because nums[0] + nums[1] = 2 + 7 = 9,
+         * return [0, 1].
+         */
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            // convert to dict, keep the index and value
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dict[nums[i]] = i;
+            }
+
+            // start process
+            int firstIndex = -1;
+            int secondIndex = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+
+                if (dict.ContainsKey(target - num))
+                {
+                    var index = dict[target - num];
+                    if (i == index)
+                    {
+                        continue;
+                    }
+
+                    firstIndex = i;
+                    secondIndex = index;
+                    break;
+                }
+            }
+
+            if (firstIndex > secondIndex)
+            {
+                return new int[] { secondIndex, firstIndex };
+            }
+
+            return new int[] { firstIndex, secondIndex };
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/TwoSumProblemTests.cs
@@ -0,0 +1,91 @@
+﻿using Xunit;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Problems.Tests
+{
+    public class TwoSumProblemTests
+    {
+        [Fact]
+        public void TwoSumTest_Case1()
+        {
+            // Arrange
+            TwoSumProblem obj = new TwoSumProblem();
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+            var expected = new int[] { 0, 1 };
+
+            // Act
+            var actual = obj.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TwoSumTest_Case2()
+        {
+            // Arrange
+            TwoSumProblem obj = new TwoSumProblem();
+            int[] nums = new int[] { 3, 2, 4 };
+            int target = 6;
+            int[] expected = new int[] { 1, 2 };
+
+            // Act
+            var actual = obj.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TwoSumTest_Case3()
+        {
+            // Arrange
+            TwoSumProblem obj = new TwoSumProblem();
+            int[] nums = new int[] { 0, 4, 3, 0 };
+            int target = 0;
+            int[] expected = new int[] { 0, 3 };
+
+            // Act
+            var actual = obj.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TwoSumTest_Case4()
+        {
+            // Arrange
+            TwoSumProblem obj = new TwoSumProblem();
+            int[] nums = new int[] { -3, 4, 3, 90 };
+            int target = 0;
+            int[] expected = new int[] { 0, 2 };
+
+            // Act
+            var actual = obj.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TwoSumTest_Case5()
+        {
+            // Arrange
+            TwoSumProblem obj = new TwoSumProblem();
+            int[] nums = new int[] { -10, -1, -18, -19 };
+            int target = -19;
+            int[] expected = new int[] { 1, 2 };
+
+            // Act
+            var actual = obj.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
