e823bc4899a379f8e9d0a4a1210e1dd37e1d1a52
vul3yo6 <vul3yo6@hotmail.com>
2020-07-26 12:27:31 +0800

    1. create SingleNumber

---
+++ LeetCode/Problems/SingleNumberProblem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /*
+     * https://leetcode.com/problems/single-number/
+     * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+     * 
+     * Note:
+     * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+     * 
+     * Example 1:
+     * Input: [2,2,1]
+     * Output: 1
+     * 
+     * Example 2:
+     * Input: [4,1,2,1,2]
+     * Output: 4
+     */
+    public class SingleNumberProblem
+    {
+        private TrimDuplicate _duplicate = new TrimDuplicate();
+
+        public int SingleNumber(int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                _duplicate.Add(num);
+            }
+
+            return _duplicate.GetSingleNumber();
+        }
+
+        internal class TrimDuplicate
+        {
+            private HashSet<int> _set = new HashSet<int>();
+
+            public void Add(int num)
+            {
+                if (_set.Contains(num))
+                {
+                    _set.Remove(num);
+                }
+                else
+                {
+                    _set.Add(num);
+                }
+            }
+
+            public int GetSingleNumber()
+            {
+                return _set.First();
+            }
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/SingleNumberProblemTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeTests.Problems
+{
+    public class SingleNumberProblemTests
+    {
+        [Fact]
+        public void Case1()
+        {
+            // Arrange
+            SingleNumberProblem obj = new SingleNumberProblem();
+            int[] num = new int[] { 2, 2, 1 };
+            int expected = 1;
+
+            // Act
+            int actual = obj.SingleNumber(num);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            // Arrange
+            SingleNumberProblem obj = new SingleNumberProblem();
+            int[] num = new int[] { 4, 1, 2, 1, 2 };
+            int expected = 4;
+
+            // Act
+            int actual = obj.SingleNumber(num);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
