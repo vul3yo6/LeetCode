a67c498b33e4c44beb866b412b020390caa17dc6
kentseng <vul3yo6@hotmail.com>
2021-02-27 13:54:11 +0800

    1. create MedianOfTwoSortedArraysProblem

---
+++ LeetCode/Problems/MedianOfTwoSortedArraysProblem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /*
+     * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+     * 
+     * Example 1:
+     * Input: nums1 = [1,3], nums2 = [2]
+     * Output: 2.00000
+     * Explanation: merged array = [1,2,3] and median is 2.
+     * 
+     * Example 2:
+     * Input: nums1 = [1,2], nums2 = [3,4]
+     * Output: 2.50000
+     * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+     * 
+     * Example 3:
+     * Input: nums1 = [0,0], nums2 = [0,0]
+     * Output: 0.00000
+     * 
+     * Example 4:
+     * Input: nums1 = [], nums2 = [1]
+     * Output: 1.00000
+     * 
+     * Example 5:
+     * Input: nums1 = [2], nums2 = []
+     * Output: 2.00000
+     */
+    public class MedianOfTwoSortedArraysProblem
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] mergedArray = nums1.Concat(nums2).ToArray();
+
+            if (mergedArray.Length == 0)
+            {
+                return 0;
+            }
+            else if (mergedArray.Length == 1)
+            {
+                return mergedArray[0];
+            }
+
+            Array.Sort(mergedArray);
+
+            bool needCalculate = mergedArray.Length % 2 == 0;
+            int indexOfMiddle = mergedArray.Length / 2;
+            if (needCalculate)
+            {
+                return (mergedArray[indexOfMiddle - 1] + mergedArray[indexOfMiddle]) / 2d;
+            }
+
+            return mergedArray[indexOfMiddle];
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/MedianOfTwoSortedArraysProblemTests.cs
@@ -0,0 +1,93 @@
+﻿using Xunit;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static LeetCode.Problems.AddTwoNumbersProblem;
+
+namespace LeetCodeTests.Problems
+{
+    public class MedianOfTwoSortedArraysProblemTests
+    {
+        [Fact]
+        public void FindMedianSortedArrays_Case1()
+        {
+            // Arrange
+            var obj = new MedianOfTwoSortedArraysProblem();
+            int[] nums1 = new int[] { 1, 3 };
+            int[] nums2 = new int[] { 2 };
+            double expected = 2;   // merged array = [1,2,3] and median is 2.
+
+            // Act
+            double actual = obj.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindMedianSortedArrays_Case2()
+        {
+            // Arrange
+            var obj = new MedianOfTwoSortedArraysProblem();
+            int[] nums1 = new int[] { 1, 2 };
+            int[] nums2 = new int[] { 3, 4 };
+            double expected = 2.5;   // merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+            // Act
+            double actual = obj.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindMedianSortedArrays_Case3()
+        {
+            // Arrange
+            var obj = new MedianOfTwoSortedArraysProblem();
+            int[] nums1 = new int[] { 0, 0 };
+            int[] nums2 = new int[] { 0, 0 };
+            double expected = 0;
+
+            // Act
+            double actual = obj.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindMedianSortedArrays_Case4()
+        {
+            // Arrange
+            var obj = new MedianOfTwoSortedArraysProblem();
+            int[] nums1 = new int[] { };
+            int[] nums2 = new int[] { 1 };
+            double expected = 1;
+
+            // Act
+            double actual = obj.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FindMedianSortedArrays_Case5()
+        {
+            // Arrange
+            var obj = new MedianOfTwoSortedArraysProblem();
+            int[] nums1 = new int[] { 2 };
+            int[] nums2 = new int[] { };
+            double expected = 2;
+
+            // Act
+            double actual = obj.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
