34772ae72830a7c109123d8b2c62a332e4c30bcc
kentseng <vul3yo6@hotmail.com>
2021-04-14 23:24:42 +0800

    1. 新增 GenerateParenthesesProblem

---
+++ LeetCode/Problems/GenerateParenthesesProblem.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Problems
+{
+    /*
+     * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+     * 
+     * Example 1:
+     * Input: n = 3
+     * Output: ["((()))","(()())","(())()","()(())","()()()"]
+     * 
+     * Example 2:
+     * Input: n = 1
+     * Output: ["()"]
+     * 
+     * Constraints:
+     * 1 <= n <= 8
+     */
+    public class GenerateParenthesesProblem
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> combinations = new List<string>();
+            GenerateAll(new char[2 * n], 0, combinations);
+            return combinations;
+        }
+
+        private void GenerateAll(char[] current, int pos, List<string> result)
+        {
+            if (pos == current.Length)
+            {
+                if (Valid(current))
+                    result.Add(new string(current));
+            }
+            else
+            {
+                current[pos] = '(';
+                GenerateAll(current, pos + 1, result);
+                current[pos] = ')';
+                GenerateAll(current, pos + 1, result);
+            }
+        }
+
+        private bool Valid(char[] current)
+        {
+            int balance = 0;
+            foreach (char c in current)
+            {
+                if (c == '(') balance++;
+                else balance--;
+                if (balance < 0) return false;
+            }
+            return (balance == 0);
+        }
+    }
+}
---
+++ LeetCodeTests/Problems/GenerateParenthesesProblemTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static LeetCode.Problems.AddTwoNumbersProblem;
+using LeetCode.Models;
+
+namespace LeetCodeTests.Problems
+{
+    public class GenerateParenthesesProblemTests
+    {
+        [Fact]
+        public void GenerateParenthesesProblem_Case1()
+        {
+            // Arrange
+            var obj = new GenerateParenthesesProblem();
+            int n = 3;
+            var expected = new List<string>()
+            {
+                "((()))",
+                "(()())",
+                "(())()",
+                "()(())",
+                "()()()"
+            };
+
+            // Act
+            IList<string> actual = obj.GenerateParenthesis(n);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GenerateParenthesesProblem_Case2()
+        {
+            // Arrange
+            var obj = new GenerateParenthesesProblem();
+            int n = 1;
+            var expected = new List<string>()
+            {
+                "()"
+            };
+
+            // Act
+            IList<string> actual = obj.GenerateParenthesis(n);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
